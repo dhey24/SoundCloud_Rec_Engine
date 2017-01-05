72955973d70125f4186d9219effbbc2f461efa6a
David Hey <davidhey@Davids-MacBook-Air.local>
2017-01-04 20:29:22 -0500

    cleaning up code

--- secondDegree_network_graph.py
+++ secondDegree_network_graph.py
@@ -16,10 +16,13 @@ def follows_follows(userid, follow_type):
     """
     A userid create their 2nd degree network
     follow type must be "followings" or "followers"
-    Followings will provide reccomendations of who a user should follow but doesnt... intended for those consuming music
-    Followers will provide similar artists to a given user id... intended for those producing music
+    Followings will provide reccomendations of who a user should follow but 
+    doesnt... intended for those consuming music
+    Followers will provide similar artists to a given user id... intended for 
+    those producing music
     
-    Always looks at the followings of the follow type provided to get the 2nd degree network
+    Always looks at the followings of the follow type provided to get the 2nd 
+    degree network
     """
     #Authenticate general purpose
     client = create_client(CLIENTID)
@@ -41,7 +44,8 @@ def follows_follows(userid, follow_type):
     
     nxt = True
     try:
-        myfollows = client.get('/users/' + str(userid) + '/' + follow_type, limit = page_size)
+        myfollows = client.get('/users/' + str(userid) + '/' + follow_type, 
+                               limit = page_size)
     except:
         print "504 Error...skipping"
         nxt = False
@@ -64,7 +68,8 @@ def follows_follows(userid, follow_type):
     #loop through all followings and get their followings
     nxt = True
     try:
-        myfollows = client.get('/users/' + str(userid) + '/' + follow_type, limit = page_size)
+        myfollows = client.get('/users/' + str(userid) + '/' + follow_type, 
+                               limit = page_size)
     except:
         print "504 Error...skipping"
         nxt = False
@@ -85,7 +90,8 @@ def follows_follows(userid, follow_type):
             
             nxt2 = True
             try:
-                myfollows2 = client.get(getstr, filter = "all", limit = page_size)
+                myfollows2 = client.get(getstr, filter = "all", 
+                                        limit = page_size)
             except:
                 print "504 Error...skipping"
                 nxt2 = False
@@ -118,7 +124,8 @@ def follows_follows(userid, follow_type):
                         nxt2 = False
             followcount += 1
             print len(edgeList), "second degree follows"
-            print followcount, "out of", len(myFollowsList), "complete...", processed, "processed" #for debugging
+            print followcount, "out of", len(myFollowsList), "complete...", 
+                  processed, "processed" #for debugging
         if myfollows.next_href == None:
             nxt = False
         else:
@@ -147,13 +154,14 @@ def reccomendationNetwork(user_id, repull_data):
     Will create a png of the network
 
     user_id = user id to get recommendations for (string)
-    repull_data = 1 to reputt the follower data, 0 if it already has been done (saves time)
+    repull_data = 1 to reputt the follower data, 0 if it already has been done 
+    (saves time)
     """
     #repull data?
     repull = repull_data
     if repull == 1:
         #find a user by username
-        new_user = searchUser("https://soundcloud.com/" + str(user_id), True) #oliver-stahlmann-195515684
+        new_user = searchUser("https://soundcloud.com/" + str(user_id), True)
         #create their second degree dict
         outtuple = follows_follows(new_user.id, "followings")
         newEdgeList = outtuple[0]
@@ -212,7 +220,8 @@ def reccomendationNetwork(user_id, repull_data):
 
     toRemove = []
     for n, d in G.nodes_iter(data = True):
-        if d["username"] == new_user.username or (d["first_degree_follow"] == False and G.degree(n) < countLimit):
+        if d["username"] == new_user.username or  \
+          (d["first_degree_follow"] == False and G.degree(n) < countLimit):
             toRemove.append(n)
     G.remove_nodes_from(toRemove)
 
@@ -232,7 +241,8 @@ def reccomendationNetwork(user_id, repull_data):
             G.node[e[0]]["recEdges"] += 1
             G.node[e[1]]["recEdges"] += 1
             
-    print len(boringEdges), "Boring edges", len(interestingEdges), "Interesting edges"
+    print len(boringEdges), "Boring edges", 
+          len(interestingEdges), "Interesting edges"
     #G.remove_edges_from(edges2remove)
 
     #remove nodes with no rec edges (cuts down massive networks)
@@ -258,7 +268,8 @@ def reccomendationNetwork(user_id, repull_data):
             G.node[e[0]]["recEdges"] += 1
             G.node[e[1]]["recEdges"] += 1
             
-    print len(boringEdges), "Boring edges", len(interestingEdges), "Interesting edges"
+    print len(boringEdges), "Boring edges", 
+          len(interestingEdges), "Interesting edges"
     nodesize = []
     nodeColor = []
     nodeLabels_f = {}
@@ -297,31 +308,42 @@ def reccomendationNetwork(user_id, repull_data):
 
 
     #nodes
-    nx.draw_networkx_nodes(G, pos, alpha = .5, node_color = nodeColor, node_size = 1) #, node_size = nodeSize
+    nx.draw_networkx_nodes(G, pos, alpha = .5, 
+                           node_color = nodeColor, 
+                           node_size = 1)
     #edges
     nx.draw_networkx_edges(G, pos, alpha = .15, edgelist = boringEdges)
-    nx.draw_networkx_edges(G, pos, alpha = .4, edgelist = interestingEdges, edge_color = '#377eb8')
+    nx.draw_networkx_edges(G, pos, alpha = .4, edgelist = interestingEdges, 
+                           edge_color = '#377eb8')
     #label
-    nx.draw_networkx_labels(G, pos, labels = nodeLabels_f, font_weight = "bold", alpha = .6)
-    nx.draw_networkx_labels(G, pos, labels = nodeLabels_nf[1], font_weight = "bold", font_color = '#bd0026', alpha = .8, font_size = 20)
-    nx.draw_networkx_labels(G, pos, labels = nodeLabels_nf[2], font_weight = "bold", font_color = '#f03b20', alpha = .8, font_size = 16)
-    nx.draw_networkx_labels(G, pos, labels = nodeLabels_nf[3], font_weight = "bold", font_color = '#fd8d3c', alpha = .8, font_size = 12)
-    nx.draw_networkx_labels(G, pos, labels = nodeLabels_nf[4], font_weight = "bold", font_color = '#feb24c', alpha = .8, font_size = 8)
-    #sequential color scheme
-    #ffffb2
-    #fecc5c
-    #fd8d3c
-    #e31a1c
-
-    #d7191c
-    #fdae61
-    #abd9e9
-    #2c7bb6
-
-    #feb24c
-    #fd8d3c
-    #f03b20
-    #bd0026
+    nx.draw_networkx_labels(G, pos, 
+                            labels = nodeLabels_f, 
+                            font_weight = "bold", 
+                            alpha = .6)
+    nx.draw_networkx_labels(G, pos, 
+                            labels = nodeLabels_nf[1], 
+                            font_weight = "bold", 
+                            font_color = '#bd0026', 
+                            alpha = .8, 
+                            font_size = 20)
+    nx.draw_networkx_labels(G, pos, 
+                            labels = nodeLabels_nf[2], 
+                            font_weight = "bold", 
+                            font_color = '#f03b20', 
+                            alpha = .8, 
+                            font_size = 16)
+    nx.draw_networkx_labels(G, pos, 
+                            labels = nodeLabels_nf[3], 
+                            font_weight = "bold", 
+                            font_color = '#fd8d3c', 
+                            alpha = .8, 
+                            font_size = 12)
+    nx.draw_networkx_labels(G, pos, 
+                            labels = nodeLabels_nf[4], 
+                            font_weight = "bold", 
+                            font_color = '#feb24c', 
+                            alpha = .8, 
+                            font_size = 8)
     plt.axis('off')
     plt.savefig("secondDegree_"+ new_user.username + "_+" + str(countLimit) +".png")
     plt.show
